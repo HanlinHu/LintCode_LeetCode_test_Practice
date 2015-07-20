0a24a9bc20ebe56e4339de2b1319431f9fab74bc
HanlinHu <HanlinHu@users.noreply.github.com>
2015-07-20 12:51:04 -0600

    Update invertBinaryTree.cpp

--- invertBinaryTree.cpp
+++ invertBinaryTree.cpp
@@ -30,13 +30,45 @@ public:
     void invertBinaryTree(TreeNode *root) {
         // write your code here
         TreeNode *temp;
+        //swap the subNodes
         temp = root->left;
         root->left = root->right;
         root->right = temp;
         
+        //do recusive job by calling recusive function
         if(root->left != nullptr)
             invertBinaryTree(root->left);
         if(root->right != nullptr)
             invertBinaryTree(root->right);    
     }
 };
+
+/*
+* Second solution
+* by using stack for doing recursive job
+*/
+    void invertBinaryTree(TreeNode *root) 
+    {
+        // write your code here
+        stack<TreeNode*> T;
+        T.emplace(root);
+        while(!T.empty())
+        {
+            //read top -- root-- and pop
+            TreeNode* node = T.top();
+            T.pop();
+            
+            TreeNode* temp;
+            
+            //swap subNodes
+            temp = node->left;
+            node->left = node->right;
+            node->right = temp;
+            
+            //use stack for recusive job
+            if(node->left != nullptr)
+                T.emplace(node->left);
+            if(node->right != nullptr)
+                T.emplace(node->right);
+        }
+    }
