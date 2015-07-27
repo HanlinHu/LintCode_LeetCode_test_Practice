b3ba8d9de48557706d78c832404eb84e17b37d1e
HanlinHu <HanlinHu@users.noreply.github.com>
2015-07-27 12:56:50 -0600

    Create reverseLinkedList.cpp

---
+++ cpp/reverseLinkedList.cpp
@@ -0,0 +1,51 @@
+/*
+*  name: reverse linked list
+*  Example:
+*  For linked list 1->2->3, the reversed linked list is 3->2->1
+*  Challenge:
+*  Reverse it in-place and in one-pass
+*/
+
+/**
+ * Definition of ListNode
+ * 
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ * 
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    ListNode *reverse(ListNode *head) 
+    {
+    
+        //special case head == nullptr;
+        
+        ListNode *current(head);
+        ListNode *newHead(nullptr);
+        ListNode *next(nullptr);
+        if(head!=nullptr)
+        {
+            while(current != nullptr)
+            {
+                next = current->next;
+                current->next = newHead;
+                newHead = current;
+                current = next;
+            }
+            return newHead;
+        }
+        else
+        return nullptr;
+    }
+};
