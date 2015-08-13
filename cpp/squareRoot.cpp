ac587986a710488374a557ae8343fb5636421c39
HanlinHu <HanlinHu@users.noreply.github.com>
2015-08-12 20:15:56 -0600

    Create squareRoot.cpp

---
+++ cpp/squareRoot.cpp
@@ -0,0 +1,40 @@
+/*
+* name: Sqrt(x)
+*
+* description: 
+* Implement int sqrt(int x).
+* Compute and return the square root of x.
+*
+* example:
+* sqrt(3) = 1
+* sqrt(4) = 2
+* sqrt(5) = 2
+* sqrt(10) = 3
+*
+* challenge:
+* O(log(x))
+*
+*
+*/
+
+
+
+
+// if use this way, we will running out of time
+class Solution {
+public:
+    /**
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
+    int sqrt(int x) 
+    {
+        // write your code here
+    
+        int m(0);
+        while((m^2)<=x)
+        ++m;
+       
+       return m;     
+    }
+};
