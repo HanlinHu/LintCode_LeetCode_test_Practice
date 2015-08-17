8286723b08f4c561007aaf0a438c3ac771881783
HanlinHu <HanlinHu@users.noreply.github.com>
2015-08-16 19:50:12 -0600

    Update squareRoot.cpp

--- cpp/squareRoot.cpp
+++ cpp/squareRoot.cpp
@@ -17,7 +17,7 @@
 *
 */
 
-
+// This question ask for enumerate solution.
 
 
 // if use this way, we will running out of time
@@ -39,7 +39,8 @@ public:
     }
 };
 
-
+// those solutions are from :
+// 
 class Solution {
 public:
     /**
@@ -61,5 +62,53 @@ public:
         }
 
         return left - 1;
+    }
+};
+
+class Solution2 {
+public:
+    /**
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
+    int sqrt(int x) {
+        int left = 1;
+        int right = x / 2 + 1;
+
+        // Find min of left s.t. left > x / left.
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid > x / mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left - 1;
+    }
+};
+
+class Solution3 {
+public:
+    /**
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
+    int sqrt(int x) {
+        int left = 0;
+        int right = x / 2 + 1;
+
+        // Find min of right s.t. right > x / right.
+        while (right - left > 1) {
+            int mid = left + (right - left) / 2;
+            if (mid > x / mid) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        return right - 1;
     }
 };
