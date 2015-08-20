47d777fd6814e85c282a3a4c6852d6e084cb90eb
HanlinHu <HanlinHu@users.noreply.github.com>
2015-08-19 19:02:24 -0600

    Update convertSortedArrayToBinarySearchTreeWithMinimalHeight.cpp

--- cpp/convertSortedArrayToBinarySearchTreeWithMinimalHeight.cpp
+++ cpp/convertSortedArrayToBinarySearchTreeWithMinimalHeight.cpp
@@ -10,3 +10,23 @@
  *     }
  * }
  */
+class Solution {
+public:
+    /**
+     * @param A: A sorted (increasing order) array
+     * @return: A tree node
+     */
+    TreeNode *sortedArrayToBST(vector<int> &A) {
+        return sortedArrayToBSTHelper(A, 0, A.size() - 1);
+    }
+
+    TreeNode *sortedArrayToBSTHelper(vector<int> &A, int start, int end) {
+        if (start <= end) {
+            TreeNode *node = new TreeNode(A[start + (end - start) / 2]);
+            node->left = sortedArrayToBSTHelper(A, start, start + (end - start) / 2 - 1);
+            node->right = sortedArrayToBSTHelper(A, start + (end - start) / 2 + 1, end);
+            return node;
+        }
+        return nullptr;
+    }
+};
